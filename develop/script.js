7786f0fe371a321e47bc7ae8e42636de77664706
Arlene Sanchez <arlene.sanchez004@gmail.com>
2020-02-03 09:13:25 -0500

    time blocks created

--- develop/script.js
+++ develop/script.js
@@ -1,5 +1,7 @@
 var currentDay = document.getElementById("currentDay");
 currentDay.textContent = moment().format("dddd, MMMM Do YYYY, h:mm:ss a"); 
-console.log(currentDay);
-//moment(testDate).format('MM/DD/YYYY');
-//
+console.log(currentDay)
+
+
+
+
