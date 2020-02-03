544a59b76840366510663f7ed7a1276413faaa2a
Arlene Sanchez <arlene.sanchez004@gmail.com>
2020-02-03 12:07:35 -0500

    work in progress: making time blocks change color according to current hour

--- develop/script.js
+++ develop/script.js
@@ -1,7 +1,28 @@
 var currentDay = document.getElementById("currentDay");
-currentDay.textContent = moment().format("dddd, MMMM Do YYYY, h:mm:ss a"); 
-console.log(currentDay)
+currentDay.textContent = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+//console.log(currentDay)
 
+//check to see what time it is, color code timeblocks
+console.log(moment().hour());
 
+var h = moment().hour();
+var hours = ["9", "10", "11", "12", "1", "2", "3", "4", "5"];
 
+$(".taskBlock").each(function (i, item) {
+    if (h === hours[i]) {
+        this.addClass("present");
+    }
+})
+// $(".taskBlock").each(function () {
+//     if (h <= hours) {
+//         $(".taskBlock").addClass("past");
+//         console.log("past");
+//     }
+// });
+
+// $(".taskBlock").each(function () {
+//     if (h >= hours) {
+//         $(".taskBlock").addClass("future");
+//     }
+// });
 
