06caa08a0a1b478f722abc330ccc6493124730fa
Arlene Sanchez <arlene.sanchez004@gmail.com>
2020-02-03 22:44:04 -0500

    can store tasks to local storage

--- develop/script.js
+++ develop/script.js
@@ -1,9 +1,9 @@
 var currentDay = document.getElementById("currentDay");
-currentDay.textContent = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+currentDay.textContent = moment().format("dddd, MMMM Do YYYY");
 //console.log(currentDay)
 
 //check to see what time it is, color code timeblocks
-console.log(moment().hour());
+//console.log(moment().hour());
 
 var h = moment().hour();
 var hours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
@@ -22,14 +22,18 @@ $(".taskBlock").each(function (i, item) {
     if (h > hours[i]) {
         $(this).addClass("future");
     }
-})
+});
 
-//when .taskBlock is clicked, input field is available
-$(".taskBlock").click(function(){
-    $(this).prop(enabled);
+//save the input
+var storageInput = ["9","10","11","12","13","14","15","16","17"];
 
-})
+$(".saveBtn").click(function(event) {
+   var storeInput = $(this).siblings("textarea").val();
+    localStorage.setItem($(this).siblings("textarea").attr("id") ,storeInput);                
+    console.log(storeInput);
+});
 
-
-
+storageInput.forEach(function(input){
+    $("#" + input).val(localStorage.getItem(input));
+});
 
